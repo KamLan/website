e330d12ee279680e70096effb088df6e019823ea
Unknown <adouardcamille@hotmail.com>
2016-11-06 10:17:57 +0100

    Fix topband + sweetalter2

--- index.php
+++ index.php
@@ -27,15 +27,15 @@
                 </strong>
               </h2>
               <h3>
-                Dr Gérard <strong>BARRAL</strong>, Dr Franck <strong>JACQUOT</strong>, Dr Irina <strong>HERGHEA</strong>, Marlène <strong>BARRAL</strong>, Sage-femme
+                Dr Gérard <strong>BARRAL</strong>, Dr Franck <strong>JACQUOT</strong>, Dr Irina <strong>HERGHEA</strong>, <br> Marlène <strong>BARRAL</strong>
               </h3>
             </div>
             <div class="col-sm-4">
               <h3>
-                <strong>Tel:</strong> 04.78.59.80.51
+                12 chemin de la croix Pivort, 69110, <strong>Sainte-Foy-Lès-Lyon</strong>
               </h3>
               <h3>
-                12 chemin de la croix Pivort, 69110, <strong>Sainte-Foy-Lès-Lyon</strong>
+                <strong>Tel:</strong> 04.78.59.80.51
               </h3>
             </div>
           </div>
